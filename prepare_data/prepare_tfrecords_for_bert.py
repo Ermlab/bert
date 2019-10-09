e64a3c60ab45b0f85a7d35ed6f01ff520a0e202e
Juliusz Lewalski <juliusz.lewalski@gmail.com>
2019-10-09 16:30:30 +0200

    hotfix wrong positional parameters

--- prepare_data/prepare_tfrecords_for_bert.py
+++ prepare_data/prepare_tfrecords_for_bert.py
@@ -102,6 +102,7 @@ if __name__ == '__main__':
                         type=int,
                         default=0,
                         help='Run from specific iteration number')
+
     args = parser.parse_args()
 
     EXTRACTION_FACTORY = {
--- runners/half_runner.sh
+++ runners/half_runner.sh
@@ -2,7 +2,6 @@
 
 
 
-./runner_1a.sh wiki_content tfrecords_seqlen_128 lower_lower
-./runner_1b.sh wiki_content tfrecords_seqlen_128 lower_lower
-./runner_2b.sh wiki_content tfrecords_seqlen_512 lower_lower
-./runner_2b_eval.sh poleval tfrecords_test lower_lower
+./runner_1a.sh wiki_content tfrecords_seqlen_128_reversed_prefix_vocab lower_lower
+./runner_1b.sh wiki_content tfrecords_seqlen_128_reversed_prefix_vocab lower_lower
+./runner_2b.sh wiki_content tfrecords_seqlen_512_reversed_prefix_vocab lower_lower

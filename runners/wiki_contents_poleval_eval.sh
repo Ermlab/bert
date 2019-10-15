c73d0bce3cb5aac5e3b21e92d8bdeaaf02b17373
Juliusz Lewalski <juliusz.lewalski@gmail.com>
2019-10-15 15:48:42 +0200

    add runner for evaulation wiki_contents with poleval test dataset

---
+++ runners/wiki_contents_poleval_eval.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+python3 ../run_pretraining.py \
+ --input_file=gs://nlp-data-storage/poleval/tfrecords/tfrecords_test/bert_dataset.tfrecords* \
+ --init_checkpoint=gs://nlp-data-storage/wiki_content/checkpoints/without_next_sentence_2b/bert_model-30000.ckpt \
+ --output_dir=gs://nlp-data-storage/wiki_content/poleval_eval \
+ --do_next_sentence_pred=True \
+ --do_train=False \
+ --do_eval=True \
+ --bert_config_file=gs://nlp-data-storage/wiki_content/config.json \
+ --eval_batch_size=128 \
+ --max_seq_length=128 \
+ --iterations_per_loop=128\
+ --max_eval_steps=128 \
+ --save_summary_steps=128 \
+ --use_tpu=True \
+ --tpu_name=polish-nlp-tpu-1-v3 \
+ --tpu_zone=us-central1-a
